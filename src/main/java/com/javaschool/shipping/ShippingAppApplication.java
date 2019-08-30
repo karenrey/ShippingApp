eef2924fa52829a9e89bfa0e06f0023a6039bd68
karenrey <michelle.rey.rodriguez@gmail.com>
2019-08-29 23:17:57 -0600

    SHP-01 Add the endpoints for type, size, time and transport.

--- src/main/java/com/javaschool/shipping/ShippingAppApplication.java
+++ src/main/java/com/javaschool/shipping/ShippingAppApplication.java
@@ -3,7 +3,7 @@ package com.javaschool.shipping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.javaschool"})
 public class ShippingAppApplication {
 
 	public static void main(String[] args) {
